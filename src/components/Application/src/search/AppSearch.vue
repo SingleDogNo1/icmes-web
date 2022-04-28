975271e829bbbbc4ab4880e38aefa339056ee93e
lost-dream <13623673118@163.com>
2022-04-28 10:02:39 +0800

    feat: 默认打开搜索功能,ctrl+shift+f 打开搜索框

--- src/components/Application/src/search/AppSearch.vue
+++ src/components/Application/src/search/AppSearch.vue
@@ -1,33 +1,38 @@
-<script lang="tsx">
-  import { defineComponent, ref, unref } from 'vue';
+<template>
+  <div class="p-1" @click="changeModal(true)">
+    <Tooltip>
+      <template #title>{{ t('common.searchText') }}</template>
+      <SearchOutlined />
+    </Tooltip>
+    <AppSearchModal @close="changeModal(false)" :visible="showModal" />
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'AppSearch',
+  };
+</script>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
   import { Tooltip } from 'ant-design-vue';
+  import { useMagicKeys, whenever } from '@vueuse/core';
   import { SearchOutlined } from '@ant-design/icons-vue';
   import AppSearchModal from './AppSearchModal.vue';
   import { useI18n } from '/@/hooks/web/useI18n';
 
-  export default defineComponent({
-    name: 'AppSearch',
-    setup() {
-      const showModal = ref(false);
-      const { t } = useI18n();
+  const { t } = useI18n();
+  const keys = useMagicKeys();
 
-      function changeModal(show: boolean) {
-        showModal.value = show;
-      }
+  const showModal = ref(false);
+  const shift_Ctrl_F = keys['Shift+Ctrl+F'];
 
-      return () => {
-        return (
-          <div class="p-1" onClick={changeModal.bind(null, true)}>
-            <Tooltip>
-              {{
-                title: () => t('common.searchText'),
-                default: () => <SearchOutlined />,
-              }}
-            </Tooltip>
-            <AppSearchModal onClose={changeModal.bind(null, false)} visible={unref(showModal)} />
-          </div>
-        );
-      };
-    },
+  function changeModal(show: boolean) {
+    showModal.value = show;
+  }
+
+  whenever(shift_Ctrl_F, () => {
+    changeModal(true);
   });
 </script>
