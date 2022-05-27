8c733e97a1b27af470c73f4564d129c6e895f5d7
lost-dream <13623673118@163.com>
2022-05-27 10:04:35 +0800

    feat(modal组件): 标题双击切换全屏状态

--- src/components/Modal/src/components/ModalHeader.vue
+++ src/components/Modal/src/components/ModalHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <BasicTitle :helpMessage="helpMessage">
+  <BasicTitle :helpMessage="helpMessage" @dblclick="handleDbclick">
     {{ title }}
   </BasicTitle>
 </template>
@@ -16,6 +16,14 @@
         type: [String, Array] as PropType<string | string[]>,
       },
       title: { type: String },
+    },
+    emits: ['dblclick'],
+    setup(_props, { emit }) {
+      function handleDbclick(e) {
+        e.stopPropagation(e);
+        emit('dblclick', e);
+      }
+      return { handleDbclick };
     },
   });
 </script>
