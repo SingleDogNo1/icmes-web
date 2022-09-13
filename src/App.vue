6578e6cf10d929d36572b11939fc9a80c35c7f8d
lost-dream <13623673118@163.com>
2022-09-13 16:48:36 +0800

    feat: 根据电脑系统主题切换明暗主题

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,30 @@
   import { AppProvider } from '/@/components/Application';
   import { useTitle } from '/@/hooks/web/useTitle';
   import { useLocale } from '/@/locales/useLocale';
-  // import { useWebSocket } from '/@/hooks/web/useWebSocket';
+  import { useRootSetting } from '/@/hooks/setting/useRootSetting';
+  import { updateHeaderBgColor, updateSidebarBgColor } from '/@/logics/theme/updateBackground';
+  import { updateDarkTheme } from '/@/logics/theme/dark';
+  import { ThemeEnum } from '/@/enums/appEnum';
   import 'dayjs/locale/zh-cn';
 
-  // support Multi-language
   const { getAntdLocale } = useLocale();
+  const { setDarkMode } = useRootSetting();
 
-  // useWebSocket();
-
-  // Listening to page changes and dynamically changing site titles
+  // 切换页面时更改标题
   useTitle();
+
+  // 根据系统主题自动切换亮色 / 暗色主题
+  const darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+  let handleChangeColorScheme = ({ matches: isDarkMode }: MediaQueryListEvent) => {
+    setDarkMode(isDarkMode ? ThemeEnum.DARK : ThemeEnum.LIGHT);
+    updateDarkTheme(isDarkMode ? ThemeEnum.DARK : ThemeEnum.LIGHT);
+    updateHeaderBgColor();
+    updateSidebarBgColor();
+  };
+
+  if (typeof darkMedia.addEventListener === 'function') {
+    darkMedia.addEventListener('change', handleChangeColorScheme);
+  } else if (typeof darkMedia.addListener === 'function') {
+    darkMedia.addListener(handleChangeColorScheme);
+  }
 </script>
